c53c556ccc9540267d21853eec926a682e3c3908
Aaron0126 <aaron.lin@has.hc.edu.tw>
2023-10-15 18:04:24 +0800

    yesssssir

--- src/main/java/com/example/Triangle.java
+++ src/main/java/com/example/Triangle.java
@@ -1,19 +1,26 @@
 package com.example;
 
 public class Triangle extends Shape {
-
+    public int adjacent;
+    public int opposite;
+    public int hypotenuse;
+    public Triangle (int adjacent, int opposite, int hypotenuse){
+        this.adjacent = adjacent;
+        this.opposite = opposite;
+        this.hypotenuse = hypotenuse;
+    }
     // TODO: Define attributes for the triangle
 
     @Override
     public double area() {
         // TODO: Implement the method
-        return 0;
+        return adjacent*opposite/2;
     }
 
     @Override
     public double perimeter() {
         // TODO: Implement the method
-        return 0;
+        return adjacent+opposite+hypotenuse;
     }
 }
 
